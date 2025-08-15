c634dd209fc4b9e56b461d884cd24bd852fdb472
Ashish <ashish74624@gmail.com>
2025-08-15 22:37:22 +0530

    Enhance employee forms and templates with improved styling and layout

--- emp/employee/forms.py
+++ emp/employee/forms.py
@@ -5,3 +5,8 @@ class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
         fields = ['first_name', 'last_name', 'email']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'border border-gray-300 rounded-lg p-2 w-full'}),
+            'last_name': forms.TextInput(attrs={'class': 'border border-gray-300 rounded-lg p-2 w-full'}),
+            'email': forms.EmailInput(attrs={'class': 'border border-gray-300 rounded-lg p-2 w-full'}),
+        }
