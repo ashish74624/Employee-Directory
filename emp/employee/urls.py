c1e83c7a731b22bc10a8cb58dc58e2bb9b32bb36
Ashish <ashish74624@gmail.com>
2025-08-15 20:08:00 +0530

    Add employee CRUD functionality with forms and templates

--- emp/employee/urls.py
+++ emp/employee/urls.py
@@ -5,5 +5,8 @@ app_name = 'employee_dir'
 
 urlpatterns = [
     path('', views.employee_list , name="list"),
-    path('<path:slug>', views.employee_page, name="page"),
+    path('create/', views.employee_create, name='create'),
+    path('update/<path:slug>', views.employee_update, name='update'),
+    path('delete/<path:slug>', views.employee_delete, name='delete'),
+    path('<path:slug>', views.employee_page, name="page"),  # keep this last
 ]
--- emp/employee/views.py
+++ emp/employee/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Employee
+from .forms import EmployeeForm
 
 # Create your views here.
 def employee_list(request):
@@ -10,3 +11,33 @@ def employee_list(request):
 def employee_page(request, slug):
     employee = Employee.objects.get(email=slug)
     return render(request, 'employees/employee_page.html', {'employee': employee})
+
+def employee_create(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('employee_dir:list')
+    else:
+        form = EmployeeForm()
+    return render(request, 'employees/employee_form.html', {'form': form})
+
+def employee_update(request, slug):
+    employee = Employee.objects.get(email=slug)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('employee_dir:list')
+    else:
+        form = EmployeeForm(instance=employee)
+    return render(request, 'employees/employee_update_form.html', {'form': form})
+
+def employee_delete(request, slug):
+    employee = Employee.objects.get(email=slug)
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('employee_dir:list')
+    return render(request, 'employees/employee_confirm_delete.html', {'employee': employee})
+    
+    
